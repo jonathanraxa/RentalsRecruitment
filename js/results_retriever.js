4dd19a0ab1146c10902ef6e0273eb0e201d45951
Jonathan Raxa <Jonathan Raxa>
2016-09-02 17:42:18 -0700

    user input enabled

--- js/results_retriever.js
+++ js/results_retriever.js
@@ -1,22 +1,25 @@
-function submitResults(mediaId) {
-    var licenseInfo = $('.pricing-option-' + mediaId + ':checked').val().split(':');
+function submitResults() {
+    console.log("HELLO");
+   // var licenseInfo = $('.pricing-option-' + mediaId + ':checked').val().split(':');
+    
     var requestObject = {
-        action: 'submitPurchase',
-        mediaId: mediaId,
-        title: $('#title-' + mediaId).html().replace("Title: ", "").trim(),
-        description: $('#description-' + mediaId).html(),
-        user: $('#user-' + mediaId).html(),
-        artist: $('#artist-' + mediaId).html(),
-        license: licenseInfo[0],
-        price: licenseInfo[1]
+        name: "BOB",
+        // bedrooms: ,
+        // bathrooms ,
+        price: price
+
     }
+
     $.ajax({
-        type: 'POST',
-        url: 'ajax/submitPurchase.php',
+        // type: 'POST',
+        // url: 'ajax/submitPurchase.php',
         dataType: 'JSON',
         data: requestObject,
         success: function(data) {
-            $('#license-type-' + mediaId).html(licenseInfo[0] + ' | $' + licenseInfo[1]);
+            console.log(data);
+            // $('#license-type-' + mediaId).html(licenseInfo[0] + ' | $' + licenseInfo[1]);
+            //  <h3>Property Name</h3>
+            // <span>Priced Recommenedation:</span><span id='pricing'><strong>$1,000</strong></span>
         }
     })
 }
